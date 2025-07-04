5af7c4879df2120529e29db440ab77144729ad49
DanielSerio <dserio.d@gmail.com>
2025-07-04 15:18:02 -0400

    header logo

--- src/modules/Core/components/Header/Header.tsx
+++ src/modules/Core/components/Header/Header.tsx
@@ -1,11 +1,24 @@
-import { Box } from "@mantine/core";
+import { Anchor, Box, Group, Text } from "@mantine/core";
 import { HeaderProviders } from "./Providers";
 
 export function Header() {
   return (
     <HeaderProviders>
-      <Box component="header" id="header" className="header" h={48}>
-        <div>Header</div>
+      <Box px="sm" component="header" id="header" className="header" h={48}>
+        <Anchor href="/" underline={"never"}>
+          <Group align="center" h={46} gap={4}>
+            <Text
+              size="xl"
+              variant="gradient"
+              gradient={{ from: "teal.4", to: "indigo.6" }}
+            >
+              Dan
+            </Text>
+            <Text size="xl" c="gray.5">
+              Serio
+            </Text>
+          </Group>
+        </Anchor>
       </Box>
     </HeaderProviders>
   );
